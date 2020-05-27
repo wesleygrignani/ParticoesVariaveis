8234618e7bb6df22409eaff90cd596f28bdb7073
wesleygrignani <43892745+wesleygrignani@users.noreply.github.com>
2020-05-26 21:11:50 -0300

    Update main.cpp
    Comentários adicionados.

--- main.cpp
+++ main.cpp
@@ -35,25 +35,25 @@ void gotoxy(short x, short y)           //definition of gotoxy function//
  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void primeiroInsert(queue<processo> &processos, memoria &memoria){
+void primeiroInsert(queue<processo> &processos, memoria &memoria){ //funcao dedicada ao primeiro insert na memoria
 
-    if(memoria.p.size()==0){
-    bloco block;
+    if(memoria.p.size()==0){ //verifica tamanho da memoria (inicialmente sempre sera 0)
+    bloco block;             // alocando um bloco para inserir na memoria
     block.id=1;
     block.p = processos.front();
     block.tamanho = block.p.tamanho;
     processos.pop();
 
-    memoria.p.push_back(block);
+    memoria.p.push_back(block); //adicionando o bloco na memoria
     memoria.tamanho -= block.tamanho;
 
-    block.id = 2;
+    block.id = 2;  //segundo bloco com o tamanho reduzido
     block.p.id=0;
     block.p.tamanho=0;
     block.p.tempo=0;
     block.tamanho=memoria.tamanho;
 
-    memoria.p.push_back(block);
+    memoria.p.push_back(block); // adicionando o segundo bloco na memoria
     memoria.tamanho -= block.tamanho;
 
 
@@ -68,14 +68,15 @@ void primeiroInsert(queue<processo> &processos, memoria &memoria){
 };
 
 void firstfit(queue<processo> &processos, memoria &memoria){ //primeiro bloco com tamanho suficiente
-
+// funcao dedicada a percorrer os blocos de memoria e quando achar um local com tamanho suficiente
+// para o processo ele é inserido.
     list<bloco>::iterator it;
     it = memoria.p.begin();
 
-        for (auto const& i : memoria.p) {
+        for (auto const& i : memoria.p) { //laço para percorrer a memoria
              processo processo = processos.front();
 
-            if(i.tamanho == processo.tamanho && i.p.id == 0){  //i.p.tamanho == 0 quando nao tiver nenhum processo no bloco
+            if(i.tamanho == processo.tamanho && i.p.id == 0){  //quando o tamanho do bloco é igual ao processo e o bloco esta livre
 
                 bloco block = i;
                 block.p.id = processo.id;
@@ -89,22 +90,23 @@ void firstfit(queue<processo> &processos, memoria &memoria){ //primeiro bloco co
 
                 break;
             }
-            else if(i.tamanho > processo.tamanho && i.p.id==0){
+            else if(i.tamanho > processo.tamanho && i.p.id==0){ //quando o tamanho do bloco é maior que o processo e o bloco esta livre
 
-                bloco block = i;
+                bloco block = i; //alocando bloco para o processo
                 block.p.id = processo.id;
                 block.p.tempo = processo.tempo;
                 block.p.tamanho = processo.tamanho;
                 block.tamanho = processo.tamanho;
 
-                bloco block2 = i;
+                bloco block2 = i; //bloco particionado com o espaco livre que restou.
                 block2.p.id = 0;
                 block2.p.tempo = 0;
                 block2.p.tamanho = 0;
 
                 block2.tamanho -= processo.tamanho;
 
-                it = memoria.p.erase(it);
+                //inserção na memoria principal
+                it = memoria.p.erase(it); 
                 it = memoria.p.insert(it,block2);
                 it = memoria.p.insert(it,block);
                 //memoria.p.erase(it);
@@ -117,9 +119,9 @@ void firstfit(queue<processo> &processos, memoria &memoria){ //primeiro bloco co
         }
 }
 
-void imprimir(memoria memoria){
+void imprimir(memoria memoria){ //funcao para imprimir a memoria no prompt
 int tam = 1,x=0,y=0;
-    for (auto const& i : memoria.p) {
+    for (auto const& i : memoria.p) { //percorrer a memoria
         bloco aux;
         aux.p = i.p;
         aux.id = i.id;
@@ -153,18 +155,18 @@ int tam = 1,x=0,y=0;
     }
 }
 
-void reduzirTempo(memoria &memoria){
+void reduzirTempo(memoria &memoria){ //funcao dedicada a reduzir o tempo dos processos.
     list<bloco>::iterator it;
     it = memoria.p.begin();
-    for (auto const& i : memoria.p) {
+    for (auto const& i : memoria.p) {//percorrer a memoria para reduzir o tempo de cada processo.
             bloco block = i;
 
-            if(block.p.tempo!=0){
+            if(block.p.tempo!=0){ // reduzir tempo
                 block.p.tempo -= 1;
                 it = memoria.p.erase(it);
                 it = memoria.p.insert(it,block);
 
-            }else{
+            }else{ //caso o tempo do processo esteja em 0 - suas informacoes saem do bloco
                 block.p.id = 0;
                 block.p.tempo = 0;
                 block.p.tamanho = 0;
@@ -176,11 +178,13 @@ void reduzirTempo(memoria &memoria){
     }
 }
 
-void analisarBlocos(memoria &memoria){
+void analisarBlocos(memoria &memoria){ 
+    //funcao dedicada a encontrar 2 blocos particionados que estao livres um ao lado do outro 
     list<bloco>::iterator it,it2,it3;
     it = memoria.p.begin();
     vector<int> auxiliar,tamanho;
-    for (auto const& i : memoria.p) {
+    // este laço criara um vetor e onde possuir um bloco livre marcara como 1 sua posicao
+    for (auto const& i : memoria.p) { //percorrer a memoria 
         if(i.p.id==0){
             auxiliar.push_back(1);
             tamanho.push_back(i.tamanho);
@@ -193,11 +197,13 @@ void analisarBlocos(memoria &memoria){
     it = memoria.p.begin();
     it2 = memoria.p.begin();
     it3 = memoria.p.begin();
+    //este laço analisara o vetor criado acima e onde houver a ocorrencia de dois 1s seguidos significa que 
+    //existem blocos para serem juntados.
     for (int a=0; a<auxiliar.size(); a++) {
         if((a+1)>auxiliar.size()){
             break;
         }
-        else if(auxiliar[a] ==1 && auxiliar[a+1]==1){
+        else if(auxiliar[a] ==1 && auxiliar[a+1]==1){ //condicao de blocos livres um ao lado do outro
             bloco block;
             block.p.id=0;
             block.p.tempo=0;
@@ -212,7 +218,7 @@ void analisarBlocos(memoria &memoria){
     }
 }
 
-void imprimirProcessos(queue<processo> p){
+void imprimirProcessos(queue<processo> p){ 
     int x=85,y=0;
     while(p.size()!=0){
         processo t = p.front();
@@ -233,12 +239,13 @@ void imprimirProcessos(queue<processo> p){
 
 int main()
 {
-    queue<processo> processos;
+    queue<processo> processos; //fila de processos 
     processo aux;
 
-    memoria memoria;
+    memoria memoria; //memoria principal
     list<bloco>::iterator it;
 
+    //processos sendo inseridos em uma fila (Podem ser configurados diferente)
     aux.id=1;
     aux.tempo=4;
     aux.tamanho=512;
@@ -284,6 +291,7 @@ int main()
     aux.tamanho=90;
     processos.push(aux);
 
+    //primeiro insert na memoria
     primeiroInsert(processos,memoria);
     imprimir(memoria);
     Sleep(1000);
@@ -291,8 +299,8 @@ int main()
     queue<processo> t = processos;
     imprimirProcessos(t);
 
-    while(processos.size()!=0){
-        firstfit(processos,memoria);
+    while(processos.size()!=0){ //retirar processos da fila e inserir na memoria.
+        firstfit(processos,memoria); //fistfit
         Sleep(1500);
         system("cls");
         imprimir(memoria);
@@ -300,7 +308,7 @@ int main()
         queue<processo> t = processos;
         imprimirProcessos(t);
 
-        reduzirTempo(memoria);
+        reduzirTempo(memoria); //reduzir o tempo dos processos.
         system("cls");
         imprimir(memoria);
 
@@ -308,10 +316,10 @@ int main()
 
         Sleep(1000);
         system("cls");
-        analisarBlocos(memoria);
+        analisarBlocos(memoria); //analisar os blocos livres 
         imprimir(memoria);
 
-        imprimirProcessos(t);
+        imprimirProcessos(t); //impressao dos processos ao lado da memoria.
 
     }
 
